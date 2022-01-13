ac55c426e57520782b897ae2ce985a8ec159ea81
unknown <nabilahwasimah25022000@gmail.com>
2022-01-13 20:10:27 +0700

    yuksport

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Yuk sport</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>Yuk sport </p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  
+
+</body>
+
+</html>
---
+++ page/tentang.php
@@ -0,0 +1,26 @@
+<div class="box-title">
+    <p>Tentang <b></b></p>
+</div>
+<div id="box">
+
+  <h1>Tentang</h1>
+  <table>
+    <tr>
+      <ul>
+        <td> Yuk dapatkan jersey impian mu disini! </td>
+      </ul>
+    </tr>
+    <tr>
+      <ul>
+        <td> jangan sampai jersey impianan mu jadi milik orang </td>
+      </ul>
+    </tr>
+    <tr>
+      <ul>
+        <td> Yukkk cekout sekarang. </td>
+      </ul>
+    </tr>
+
+  </table>
+
+</div>
